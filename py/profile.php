0e1ce5fe88d7042fd2051de98057409514b92ec3
Daxil <58117160+Daxil@users.noreply.github.com>
2023-07-31 19:30:52 +0300

    31.08

--- py/profile.php
+++ py/profile.php
@@ -1,43 +1,9 @@
-<?php
-include 'dbconnect.php';
-
-if(isset($_POST['go'])) {
-    $name = $_POST["name"];
-    $fullname = $_POST["fullname"];
-    $mail = $_POST["mail"];
-    $pass = $_POST["pass"];
-
-    if(!$pass || !$mail) {
-        $error = 'Вы не ввели почту или пароль';
-    }
-
-    if(!$error) {
-        $query = "SELECT * FROM users WHERE mail = '$mail' AND pass = '$pass'";
-        $result = mysqli_query($conn, $query);
-        $row = mysqli_fetch_assoc($result);
-
-        if(mysqli_num_rows($result) > 0) {
-            // User found, redirect to profile page
-            header("Location: profile.php?name=" . urlencode($row["name"]));
-            exit;
-        } else {
-            // User not found
-            $error = "Пользователь не найден";
-        }
-    } else {
-        echo $error;
-        exit;
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="css\cart.css?<?echo time();?>">
   <link rel="stylesheet" type="text/css" href="css\css.css?<?echo time();?>">
-  <link rel="stylesheet" type="text/css" href="css\login.css?<?echo time();?>">
   <link href='https://unpkg.com/css.gg@2.0.0/icons/css/search.css' rel='stylesheet'>
   <script src="https://kit.fontawesome.com/628c8d2499.js" crossorigin="anonymous"></script>
 
@@ -162,64 +128,17 @@ if(isset($_POST['go'])) {
 
   </div>
 </header>
-<form class="LogForm"  method="post">
-  <div class="soForm">
-  <!-- Email input -->
-  <div class="form-outline mb-4">
-    <input type="email"class="form-control" id="mail" name="mail">
-    <label class="form-label" for="form3Example3">Почта</label>
-  </div>
 
-  <!-- Password input -->
-  <div class="form-outline mb-4">
-    <input type="password" class="form-control" id="pass" name="pass">
-    <label class="form-label" for="form3Example4">Пароль</label>
-  </div>
-  <!-- 2 column grid layout for inline styling -->
-  <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
-      <!-- Checkbox -->
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example34" checked />
-        <label class="form-check-label" for="form2Example34"> Remember me </label>
-      </div>
-    </div>
+    <h1>Профиль</h1>
 
-    <div class="col">
-      <!-- Simple link -->
-      <a href="#!">Forgot password?</a>
-    </div>
-  </div>
+    <?php
+    $name = $_GET['name'];
 
-  <!-- Submit button -->
-    <button type="submit" name='go' class="btn btn-primary btn-block mb-4">Войти</button>
+    echo "Имя пользователя: " . htmlspecialchars($name);
+    ?>
+  </body>
 
-  <!-- Register buttons -->
-  <div class="text-center">
-    <p>Not a member? <a href="reg.php">Register</a></p>
-    <p>or sign up with:</p>
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
-
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-google"></i>
-    </button>
-
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
-    </button>
-
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-github"></i>
-    </button>
-  </div>
-
-</div>
-</form>
-</body>
-
-    <script src="js.js?<?echo time();?>"></script>
-  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</html>
+      <script src="js.js?<?echo time();?>"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </html>
